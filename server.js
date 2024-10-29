47c190200fcaf0682c43699ba472ac1117c54c72
Igor <igormartins.all@gmail.com>
2024-10-29 20:53:51 -0300

    creating the server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+app.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+  express: app
+})
+
+app.get('/', function(req, res) {
+  return res.render('index')
+})
+
+app.listen(8080, function() {
+  console.log('> server started...')
+})
